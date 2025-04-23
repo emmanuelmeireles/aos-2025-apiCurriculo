e1ca8ea9cea581f6eff9c3fb7ebffca0b6a9c604
Emmanuel Meireles Silva <emmanuelmeireles@gmail.com>
2025-04-23 20:18:48 -0300

    Delete Curriculo-API directory
    ajustes

--- Curriculo-API/src/main/java/com/exemplo/curriculoapi/repository/ExperienciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.curriculoapi.repository;
-
-import com.exemplo.curriculoapi.model.Experiencia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExperienciaRepository extends JpaRepository<Experiencia, Long> {
-}
