15f3ae8f53dd73dc6ec6da96233899a4bd2cf1b2
emmanuelmeireles <emmanuelmeireles@gmail.com>
2025-04-25 18:14:28 -0300

    colocado componetScan para escanear corretamento o controlador

--- CurriculoApi/src/main/java/com/example/CurriculoApi/CurriculoApiApplication.java
+++ CurriculoApi/src/main/java/com/example/CurriculoApi/CurriculoApiApplication.java
@@ -2,8 +2,14 @@ package com.example.CurriculoApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "controller")
+public class Application {
+}
+
 public class CurriculoApiApplication {
 
 	public static void main(String[] args) {
